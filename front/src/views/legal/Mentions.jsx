f95115d0c27dd4637848d0a2990978e54760bc81
Esscraye <esscrayebat@netcourrier.com>
2024-11-08 20:18:09 -0500

    chore(): add legal folder

---
+++ front/src/views/legal/Mentions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mentions = () => {
+    return (
+        <div>
+            <h1>Legal Mentions</h1>
+            <p>This is where you can put your legal mentions content.</p>
+        </div>
+    );
+};
+
+export default Mentions;
